75007b9fe5f83662a1288a148636a9418c10981b
Peter-from-Piter <57870142+Peter-from-Piter@users.noreply.github.com>
2019-11-22 21:38:22 +0300

    add comments

--- C-LIST/C-LIST/sl-list.h
+++ C-LIST/C-LIST/sl-list.h
@@ -18,12 +18,12 @@ typedef struct _LIST
 	struct LIST_ENTRY *first;
 } LIST;
 
-LIST *list_new();
+LIST *list_new(); //creates new list and returns pointer to the list
 
-void list_delete(LIST *list);
+void list_delete(LIST *list); //deletes all list
 
-void list_insert(LIST *l, int a);
+void list_insert(LIST *l, int a); //insert element with value a to the top of the list
 
-void list_remove(LIST *l, int a);
+void list_remove(LIST *l, int a); //removes elements with value a from the list
 
-void list_print(LIST *l);
+void list_print(LIST *l); //prints all elements of the list
